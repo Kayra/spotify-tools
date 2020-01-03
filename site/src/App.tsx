ff58a383eec9120bb97ccdd8f25cbb8c549c3c28
Kayra <KayraAlat@gmail.com>
2020-01-03 22:04:00 +0000

    Added initial mark up and cleaned up defaults

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -6,18 +6,34 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1>Spotify Toolset</h1>
+
+        <p>A set of tools to facilitate interaction with a large amount of playlists.</p>
+
+        <p>Currently features:</p>
+        <ul>
+          <li>Song duplicate check</li>
+          <li>Song timeline</li>
+          <li>Library playlist backup</li>
+        </ul>
+
+        <p>Be sure to <a href="https://twitter.com/kayraalat" target="_blank">tweet at me</a> or <a href="https://github.com/Kayra/spotify-tools/issues" target="_blank">create a Github issue</a> if there's a feature you'd like to see.</p>
+
+        <hr></hr>
+
+        <div>
+          <h2>Song Duplicate Check</h2>
+        </div>
+
+        <div>
+          <h2>Song Timeline</h2>
+        </div>
+
+        <div>
+          <h2>Library Playlist Backup</h2>
+        </div>
+
       </header>
     </div>
   );
