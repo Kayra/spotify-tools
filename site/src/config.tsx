6ef05eee16710b82a7a2e94e03f07715f11055fe
Kayra <KayraAlat@gmail.com>
2020-01-15 20:11:00 +0000

    Added config file

---
+++ site/src/config.tsx
@@ -0,0 +1,16 @@
+interface Configuration {
+    env: string;
+    spotify_token: string
+}
+
+
+export function getConfig(): Configuration {
+
+    let config: Configuration = {
+        'env': process.env.NODE_ENV || 'default',
+        'spotify_token': process.env.SPOTIFY_TOKEN || ''
+    };
+
+    return config;
+
+}
