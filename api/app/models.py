a0857c659967ffb968f766496c9e0590f8b81ab0
Kayra <KayraAlat@gmail.com>
2020-02-13 17:34:00 +0000

    Updated model

--- api/app/models.py
+++ api/app/models.py
@@ -1,42 +1,16 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 from pymongo import MongoClient
 
-from bson import ObjectId
-from typing import Optional
+from datetime import datetime
+from typing import Dict
 
 
 client = MongoClient()
-db = client.test
-
-
-class PyObjectId(ObjectId):
-
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate
-
-    @classmethod
-    def validate(cls, object_id_value):
-
-        if not ObjectId.is_valid(object_id_value):
-            raise ValueError('Invalid ObjectId value')
-
-        return object_id_value
-
-    @classmethod
-    def __modify_schema__(cls, field_schema):
-        field_schema.update(type='string')
+db = client.spotify
 
 
 class User(BaseModel):
 
-    id: Optional[PyObjectId] = Field(alias='_id')
-    name: str
     username: str
-    email: str
-
-    class Config:
-        arbitrary_types_allowed = True
-        json_encoders = {
-            ObjectId: str
-        }
+    playlist_track_mapping: Dict
+    last_updated: datetime
