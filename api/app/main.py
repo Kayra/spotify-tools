ca7d0723f3001ed88eb35c0ddc6dd0219e36e7c4
Kayra <KayraAlat@gmail.com>
2020-02-27 21:31:00 +0000

    Added initial docker stuff

--- api/app/main.py
+++ api/app/main.py
@@ -1,3 +1,5 @@
+import os
+
 from pymongo import MongoClient
 from fastapi import FastAPI
 
@@ -8,7 +10,14 @@ from views.spotify_views import router as spotify_views_router
 
 app = FastAPI()
 spotify = Spotify()
-client = MongoClient()
+
+
+mongo_host = os.environ.get('MONGO_HOST')
+mongo_port = os.environ.get('MONGO_PORT')
+mongo_user = os.environ.get('MONGO_USER')
+mongo_password = os.environ.get('MONGO_PASSWORD')
+mongo_db = os.environ.get('MONGO_DB')
+client = MongoClient(f'mongodb://{mongo_user}:{mongo_password}@{mongo_host}:{mongo_port}/{mongo_db}')
 db = client.spotify
 
 
