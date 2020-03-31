8c539f5fbbb8e81cb19d62bbcf6bfea8713fe4a4
Kayra <KayraAlat@gmail.com>
2020-03-31 21:07:00 +0100

    Added initial timeline

---
+++ client/src/Timeline.tsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import HorizontalTimeline from 'react-horizontal-timeline';
+
+interface DatePoint {
+  data: string,
+  status: string,
+  statusB: string,
+  statusE: string
+}
+
+const EXAMPLE = [
+  {
+    data: "2019-12-05",
+    status: "status",
+    statusB: "Admission Start",
+    statusE: "Admission Open"
+  },
+  {
+    data: "2020-01-21",
+    status: "status",
+    statusB: "Start 1st round",
+    statusE: "Open for Fillup"
+  },
+  {
+    data: "2020-02-25",
+    status: "status",
+    statusB: "Start 2nd round",
+    statusE: "process"
+  },
+  {
+    data: "2020-03-16",
+    status: "status",
+    statusB: "Start 3rd round",
+    statusE: "Done"
+  },
+  {
+    data: "2020-04-19",
+    status: "status",
+    statusB: "Start 4th round",
+    statusE: "Done"
+  },
+  {
+    data: "2020-05-23",
+    status: "status",
+    statusB: "Complete",
+    statusE: "Done"
+  }
+];
+
+interface IProps {
+  // dates: Array<DatePoint>
+  dates: any;
+}
+
+interface IState {
+  curIdx: number;
+  prevIdx: number;
+}
+
+export default class Timeline extends React.Component<IProps, IState> {
+    
+  constructor(props: IProps) {
+
+    super(props);
+
+    this.state = {
+      curIdx: 0,
+      prevIdx: -1
+    };
+
+  }
+
+  render() {
+
+    const { curIdx, prevIdx } = this.state;
+    const curStatus = this.props.dates[curIdx].statusB;
+    const prevStatus = prevIdx >= 0 ? this.props.dates[prevIdx].statusB : "";
+
+    return (
+      <div>
+        <div
+          style={{
+            width: "80%",
+            height: "100px",
+            margin: "0 auto",
+            marginTop: "20px",
+            fontSize: "15px"
+          }}
+        >
+          <HorizontalTimeline
+            styles={{
+              background: "#f8f8f8",
+              foreground: "#1A79AD",
+              outline: "#dfdfdf"
+            }}
+
+            index={this.state.curIdx}
+            indexClick={(index: number) => {
+              const curIdx = this.state.curIdx;
+              this.setState({ curIdx: index, prevIdx: curIdx });
+            }}
+
+            values={this.props.dates.map((x: DatePoint) => x.data)}
+
+          />
+        </div>
+        <div className="text-center">
+          {/* Prevoius:-{prevStatus} - Current Select:-{curStatus} */}
+          {curStatus}
+        </div>
+      </div>
+    );
+  }
+}
