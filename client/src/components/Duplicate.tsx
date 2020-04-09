444101d40896b0d3b40abe6af6f47bf4d79537bd
Kayra <KayraAlat@gmail.com>
2020-04-09 16:47:00 +0100

    Added initial duplicate component

---
+++ client/src/components/Duplicate.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import Api from '../Api';
+
+interface IProps {
+    api: Api;
+    userName: string;
+}
+
+interface IState {
+  track: string;
+  artist: string;
+  duplicatePlaylists: Array<string>;
+}
+
+export default class Duplicate extends React.Component<IProps, IState> {
+
+    // api: Api;
+    state: IState;
+
+    constructor(props: IProps) {
+        super(props);
+        // this.api = props.api;
+        this.handleTrackChange = this.handleTrackChange.bind(this);
+        this.handleArtistChange = this.handleArtistChange.bind(this);
+        this.handleDuplicateSubmit = this.handleDuplicateSubmit.bind(this);
+        this.state = {
+            track: '',
+            artist: '',
+            duplicatePlaylists: []
+        };
+    }
+
+    handleTrackChange(event: any): void {
+        this.setState({track: event.target.value});
+    }
+
+    handleArtistChange(event: any): void {
+        this.setState({artist: event.target.value});
+    }
+
+    async handleDuplicateSubmit(event: any): Promise<void> {
+        event.preventDefault();
+        const response = await this.props.api.spotifyFind(this.props.userName, this.state.track, this.state.artist);
+        this.setState({duplicatePlaylists: response.data['playlists']});
+    }
+
+    render() {
+        return (
+            <div>
+            <h2>Song Duplicate Check</h2>
+            <form onSubmit={this.handleDuplicateSubmit}>
+              <label>
+                Track: <input type="text" value={this.state.track} onChange={this.handleTrackChange} />
+                Artist: <input type="text" value={this.state.artist} onChange={this.handleArtistChange} />
+              </label>
+              <input type="submit" value="Submit" />
+            </form>
+
+            {
+              this.state.duplicatePlaylists ?
+                <ul>
+                  {this.state.duplicatePlaylists.map(playList => <li key={playList}>{playList}</li>)}
+                </ul>
+              : ""
+            }
+
+          </div>
+        )
+    }
+
+}
