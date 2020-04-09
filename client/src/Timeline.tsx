40b788403d086d9bcc6b2dc48ce835ecd081f6c5
Kayra <KayraAlat@gmail.com>
2020-04-09 16:27:00 +0100

    Updated timeline code

--- client/src/Timeline.tsx
+++ client/src/Timeline.tsx
@@ -8,44 +8,26 @@ interface DatePoint {
   statusE: string
 }
 
-const EXAMPLE = [
-  {
-    data: "2019-12-05",
-    status: "status",
-    statusB: "Admission Start",
-    statusE: "Admission Open"
-  },
-  {
-    data: "2020-01-21",
-    status: "status",
-    statusB: "Start 1st round",
-    statusE: "Open for Fillup"
-  },
-  {
-    data: "2020-02-25",
-    status: "status",
-    statusB: "Start 2nd round",
-    statusE: "process"
-  },
-  {
-    data: "2020-03-16",
-    status: "status",
-    statusB: "Start 3rd round",
-    statusE: "Done"
-  },
-  {
-    data: "2020-04-19",
-    status: "status",
-    statusB: "Start 4th round",
-    statusE: "Done"
-  },
-  {
-    data: "2020-05-23",
-    status: "status",
-    statusB: "Complete",
-    statusE: "Done"
-  }
-];
+// const EXAMPLE = [
+//   {
+//     data: "2019-12-05",
+//     status: "status",
+//     statusB: "Admission Start",
+//     statusE: "Admission Open"
+//   },
+//   {
+//     data: "2020-01-21",
+//     status: "status",
+//     statusB: "Start 1st round",
+//     statusE: "Open for Fillup"
+//   },
+//   {
+//     data: "2020-02-25",
+//     status: "status",
+//     statusB: "Start 2nd round",
+//     statusE: "process"
+//   },
+// ];
 
 interface IProps {
   // dates: Array<DatePoint>
